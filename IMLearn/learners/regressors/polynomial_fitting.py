3a72ffd9ffc4fbbb70a2f7d20a7bb507830d89b4
DoronGoldenberg <doron.goldenberg@mail.huji.ac.il>
2022-04-07 11:15:16 +0300

    Created basic polynomial fitting algorithem

--- IMLearn/learners/regressors/polynomial_fitting.py
+++ IMLearn/learners/regressors/polynomial_fitting.py
@@ -5,7 +5,7 @@ from ...base import BaseEstimator
 import numpy as np
 
 
-class PolynomialFitting(BaseEstimator):
+class PolynomialFitting(LinearRegression):
     """
     Polynomial Fitting using Least Squares estimation
     """
@@ -18,57 +18,8 @@ class PolynomialFitting(BaseEstimator):
         k : int
             Degree of polynomial to fit
         """
-        super().__init__()
-        raise NotImplementedError()
-
-    def _fit(self, X: np.ndarray, y: np.ndarray) -> NoReturn:
-        """
-        Fit Least Squares model to polynomial transformed samples
-
-        Parameters
-        ----------
-        X : ndarray of shape (n_samples, n_features)
-            Input data to fit an estimator for
-
-        y : ndarray of shape (n_samples, )
-            Responses of input data to fit to
-        """
-        raise NotImplementedError()
-
-    def _predict(self, X: np.ndarray) -> np.ndarray:
-        """
-        Predict responses for given samples using fitted estimator
-
-        Parameters
-        ----------
-        X : ndarray of shape (n_samples, n_features)
-            Input data to predict responses for
-
-        Returns
-        -------
-        responses : ndarray of shape (n_samples, )
-            Predicted responses of given samples
-        """
-        raise NotImplementedError()
-
-    def _loss(self, X: np.ndarray, y: np.ndarray) -> float:
-        """
-        Evaluate performance under MSE loss function
-
-        Parameters
-        ----------
-        X : ndarray of shape (n_samples, n_features)
-            Test samples
-
-        y : ndarray of shape (n_samples, )
-            True labels of test samples
-
-        Returns
-        -------
-        loss : float
-            Performance under MSE loss function
-        """
-        raise NotImplementedError()
+        super().__init__(False)
+        self.k_ = k
 
     def __transform(self, X: np.ndarray) -> np.ndarray:
         """
@@ -83,4 +34,4 @@ class PolynomialFitting(BaseEstimator):
         transformed: ndarray of shape (n_samples, k+1)
             Vandermonde matrix of given samples up to degree k
         """
-        raise NotImplementedError()
+        return np.vander(X, self.k_ + 1)
